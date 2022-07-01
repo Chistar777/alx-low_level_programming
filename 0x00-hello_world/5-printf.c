51a520673c809e7d7b6c809d7132708efa86dcef
chidera <chiderastar777@gmail.com>
2022-06-30 22:18:50 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdi.h>
 /**
  * main - Entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+	printf("with proper grammar, but the outcome is a picece of art,\n");
+	return (0)
 }
