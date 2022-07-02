68e04849003f7354becd53665919950db0ec38ce
chidera <chiderastar777@gmail.com>
2022-07-01 19:36:58 -0700

    8print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints all the numbers of base 16 in lowercase.
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
@@ -17,5 +17,5 @@ int main(void)
 		putchar(letter);
 
 	putchar('\n');
-	return (0)
+	return (0);
 }
