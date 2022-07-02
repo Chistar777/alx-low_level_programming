3ae3fe1127de0ee4585e1a918b77404d6abbf4f4
chidera <chiderastar777@gmail.com>
2022-07-01 18:12:03 -0700

    3print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit,
- * compare it with 5
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
  *
  * Return: 0
  */
 int main(void)
 {
-	char c = 'a';
+	char letter;
 
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	
 	putchar('\n');
+
 	return (0);
 }
