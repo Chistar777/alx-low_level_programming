86cc98be4af2833b1e79eeeb6ba4cb7aa276e08b
chidera <chiderastar777@gmail.com>
2022-07-05 16:49:55 -0700

    hot fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-/*
- * File: main.h
- * Auth: chidera emmaunel
- * Desc: Header file containing declerations for the function
- * used in the 0x02-functions_nested_loops directory
- */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
