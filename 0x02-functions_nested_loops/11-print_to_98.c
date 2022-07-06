ac55ce7a63265a6904670945b459f6239d36cbc1
chidera <chiderastar777@gmail.com>
2022-07-05 21:18:12 -0700

    11print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,33 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 to - Print al naturel number from input 98
- * in order to seprated by a comma followed by aspace.
- * @n: the number begin counting at.
+ * print_to_98 - prints to 98
+ * @n: the starting point
  */
-print_to_98(int n)
+void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-			printf("%d/n", n);
-	}
+	 if (n <= 98)
+	 {
+		 while (n <= 98)
+		 {
+			  if (n == 98)
+				  printf("%d\n", n);
+			  else
+				  printf("%d, ", n);
+			  n = n + 1;
+		 }
+	 } else if (n > 98)
+	 {
+		  while (n >= 98)
 
-	else
-	{
-		while (n > 98)
-			printf("%d, ", n++);
-			printf("%d/n", n);
-	}
+		  {
+			      if (n == 98)
+				      printf("%d\n", n);
+			      else 
+				      printf("%d, ", n);
+			       n = n - 1;
+		  }
+	 }
 }
+				  
