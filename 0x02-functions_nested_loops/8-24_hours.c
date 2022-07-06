08ecc3a136dc36a738fde93d9a6ec4406a4cc429
chidera <chiderastar777@gmail.com>
2022-07-05 20:33:59 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,6 +7,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+	
 	i = 0;
 
 	while (i < 24)
@@ -16,7 +17,7 @@ void jack_bauer(void)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			_putchar(':');									_putchar((j / 10) + '0');
+			_putchar(':');									_putchar((j / 10) + '0')
 			_putchar((j % 10) + '0');							_putchar('\n');
 			j++;
 		}
