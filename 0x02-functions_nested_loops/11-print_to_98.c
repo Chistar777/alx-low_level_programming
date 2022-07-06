01612dc215e45e9aa929974023e8ed575f9db415
chidera <chiderastar777@gmail.com>
2022-07-05 19:15:37 -0700

    fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,11 +27,11 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-		       	printf("%d", n);
-			
-			if (n == 98) 
+			printf("%d", n);
+
+			if (n == 98)
 				continue;
-			printf(", ");
+			printf(",");
 		}
 		printf("\n");
 	}
