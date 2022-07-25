9270bae9511314a67dd985e27093e0ce522435a7
chidera <chiderastar777@gmail.com>
2022-07-25 13:23:09 -0700

    fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,19 +27,18 @@ char *argstostr(int ac, char **av)
 /* allocate memory for total number of chars and
  * new line for each word
  */
-
-		strDup = malloc(sizeof(char) * size);
-		if (strDup == NULL)
-			return (NULL);
-		k = 0;
-		for (i = 0; i < ac; i++)
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			for (j = 0; av[i][j] != '\0'; j++)
-			{
-				strDup[k++] = av[i][j];
-			}
-			strDup[k++] = '\n';
+			strDup[k++] = av[i][j];
 		}
-		strDup[k] = '\0';
-		return (strDup);
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
 }
